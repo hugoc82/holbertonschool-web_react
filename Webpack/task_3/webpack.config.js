82b015eea48640ca9604295e493e00155bb808c1
hugoc82 <hugocapelle@orange.fr>
2025-09-15 16:41:35 +0200

    .

---
+++ Webpack/task_3/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    header: "./modules/header/header.js",
+    body: "./modules/body/body.js",
+    footer: "./modules/footer/footer.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  devServer: {
+    static: path.resolve(__dirname, "public"),
+    port: 8564,
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  devtool: "inline-source-map",
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Holberton Dashboard",
+    }),
+  ],
+};
