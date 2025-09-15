4be151f9c4609a8da14baf440337731a6888604f
hugoc82 <hugocapelle@orange.fr>
2025-09-15 13:34:04 +0200

    .

---
+++ Webpack/task_0/src/index.js
@@ -0,0 +1,5 @@
+import $ from "jquery";
+
+$("body").append("<p>Holberton Dashboard</p>");
+$("body").append("<p>Données du tableau de bord des étudiants</p>");
+$("body").append("<p>Copyright - Holberton School</p>");
