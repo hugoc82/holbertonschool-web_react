6cfa8b02b275e80b5d4b57442e5a8e073b13064a
hugoc82 <hugocapelle@orange.fr>
2025-09-15 14:39:28 +0200

    Move task_0 to root directory as required

--- Webpack/task_0/src/index.js
+++
@@ -1,5 +0,0 @@
-import $ from "jquery";
-
-$("body").append("<p>Holberton Dashboard</p>");
-$("body").append("<p>Données du tableau de bord des étudiants</p>");
-$("body").append("<p>Copyright - Holberton School</p>");
