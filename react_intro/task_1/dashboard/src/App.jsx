ed05dd98f6e28b22b490aaed96ae6af4d100046b
hugoc82 <hugocapelle@orange.fr>
2025-09-22 12:41:14 +0200

    .

---
+++ react_intro/task_1/dashboard/src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import './App.css';
+import logo from './assets/holberton-logo.jpg';
+import Notifications from "./Notifications";
+import { getCurrentYear, getFooterCopy } from "./utils";
+
+function App() {
+  return (
+    <>
+      <div className="root-notifications">
+        <Notifications />
+      </div>
+
+      <div className="App-header">
+        <img src={logo} alt="holberton logo" />
+        <h1>School dashboard</h1>
+      </div>
+
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+
+      <div className="App-footer">
+        <p>
+          Copyright {getCurrentYear()} - {getFooterCopy(true)}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
