79b070d2bfa1c4d2920008da86afdb512956ba0f
hugoc82 <hugocapelle@orange.fr>
2025-09-24 11:35:07 +0200

    .

---
+++ react_intro/task_5/dashboard/src/App/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./App.css";
+import logo from "./assets/holberton-logo.jpg"; // <-- important: l'import explicite
+import { getCurrentYear, getFooterCopy } from "./utils";
+import Notifications from "./Notifications";
+
+function App() {
+  return (
+    <>
+      <div className="root-notifications">
+        <Notifications />
+      </div>
+
+      <div className="App-header">
+        <img src={logo} alt="holberton logo" />
+        <h1>School dashboard</h1>
+      </div>
+
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+
+        <label htmlFor="email">Email</label>
+        <input id="email" name="email" type="email" />
+
+        <label htmlFor="password">Password</label>
+        <input id="password" name="password" type="password" />
+
+        <button type="button">OK</button>
+      </div>
+
+      <div className="App-footer">
+        <p>
+          Copyright {getCurrentYear()} - {getFooterCopy(true)}
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ react_intro/task_5/dashboard/src/Notifications/Notifications.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Notifications.css";
+import { getLatestNotification } from "./utils";
+import closeIcon from "./assets/close-button.png"; // <-- utiliser le nom "close-button.png"
+
+function Notifications() {
+  const handleClose = () => {
+    console.log("Close button has been clicked");
+  };
+
+  return (
+    <div className="notification-items">
+      <button
+        type="button"
+        aria-label="Close"
+        onClick={handleClose}
+        style={{
+          position: "absolute",
+          right: "8px",
+          top: "8px",
+          background: "transparent",
+          border: "none",
+          cursor: "pointer",
+          padding: 0,
+          lineHeight: 0,
+        }}
+      >
+        <img src={closeIcon} alt="close" style={{ width: "12px", height: "12px" }} />
+      </button>
+
+      <p>Here is the list of notifications</p>
+
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li
+          data-priority="urgent"
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        ></li>
+      </ul>
+    </div>
+  );
+}
+
+export default Notifications;
