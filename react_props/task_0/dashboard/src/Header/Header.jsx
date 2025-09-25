7fc25dee34843cd59a6eb8d4fda90a3cd24035b7
hugoc82 <hugocapelle@orange.fr>
2025-09-25 11:55:36 +0200

    Remove image import from Header for Node runner

--- react_props/task_0/dashboard/src/Header/Header.jsx
+++ react_props/task_0/dashboard/src/Header/Header.jsx
@@ -1,10 +1,9 @@
 ﻿import "./Header.css";
-import logo from "../assets/holberton-logo.jpg";
 
 export default function Header() {
   return (
     <header className="app-header">
-      <img src={logo} alt="Holberton School logo" className="logo" />
+      {/* Pas d'import d'image ici pour éviter d'exploser en Node */}
       <h1>School dashboard</h1>
     </header>
   );
