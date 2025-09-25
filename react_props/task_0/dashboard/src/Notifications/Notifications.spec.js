93725a34c04ac7a7adaa103aff41159cd7bb1305
hugoc82 <hugocapelle@orange.fr>
2025-09-25 11:14:55 +0200

    Add Notifications.spec.js

---
+++ react_props/task_0/dashboard/src/Notifications/Notifications.spec.js
@@ -0,0 +1,8 @@
+﻿import { render } from "@testing-library/react";
+import Notifications from "./Notifications";
+
+describe("Notifications", () => {
+  it("render sans crash", () => {
+    render(<Notifications />);
+  });
+});
