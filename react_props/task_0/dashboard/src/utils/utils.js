8cb2c609b0ec22a23237592048f6df4a4bb90bac
hugoc82 <hugocapelle@orange.fr>
2025-09-25 11:21:14 +0200

    Add utils utils.js and wire into Footer

---
+++ react_props/task_0/dashboard/src/utils/utils.js
@@ -0,0 +1,7 @@
+﻿export const getFullYear = () => new Date().getFullYear();
+
+export const getFooterCopy = (isIndex) =>
+  isIndex ? "Holberton School" : "Holberton School main dashboard";
+
+export const getLatestNotification = () =>
+  "<strong>Urgent requirement</strong> - complete by EOD";
