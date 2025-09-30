7f53e03b83f27b432c233eb30c6ca3a167f22a44
hugoc82 <hugocapelle@orange.fr>
2025-09-30 11:25:39 +0200

    .

--- react_props/task_0/dashboard/src/App/App.jsx
+++ react_props/task_0/dashboard/src/App/App.jsx
@@ -1,1 +1,19 @@
-﻿export { default } from "./App.js";
+﻿@'
+import React, { Fragment } from "react";
+
+import Notifications from "../Notifications/Notifications";
+import Header from "../Header/Header";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+
+export default function App() {
+  return (
+    <Fragment>
+      <Notifications />
+      <Header />
+      <Login />
+      <Footer />
+    </Fragment>
+  );
+}
+'@ | Set-Content -Encoding UTF8 .\src\App\App.jsx
