090ee337aeb28824ea574936ea299fefa0e5820b
hugoc82 <hugocapelle@orange.fr>
2025-09-30 11:11:46 +0200

    .

--- react_props/task_0/dashboard/src/App/App.js
+++ react_props/task_0/dashboard/src/App/App.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import "./App.css";
-import Header from "../Header/Header.js";
-import Login from "../Login/Login.js";
+import Header from "../Header/Header.jsx";
+import Login from "../Login/Login.jsx";
 import Footer from "../Footer/Footer.js";
 import Notifications from "../Notifications/Notifications.js";
 
