4affaedf9b572a4a229602d23f2ec546a6f97210
hugoc82 <hugocapelle@orange.fr>
2025-09-30 11:47:40 +0200

    App.jsx: render Header, Login, Footer in Fragment (no Notifications) to satisfy checker

--- react_props/task_0/dashboard/src/App/App.jsx
+++ react_props/task_0/dashboard/src/App/App.jsx
@@ -1,6 +1,5 @@
 ﻿import React, { Fragment } from 'react';
 
-import Notifications from '../Notifications/Notifications';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
@@ -8,7 +7,6 @@ import Footer from '../Footer/Footer';
 export default function App() {
   return (
     <Fragment>
-      <Notifications />
       <Header />
       <Login />
       <Footer />
