a6a515aa284c9c86eb079ef28c43f3a88971f8a9
hugoc82 <hugocapelle@orange.fr>
2025-09-30 11:14:35 +0200

    .

--- react_props/task_0/dashboard/src/App/App.js
+++ react_props/task_0/dashboard/src/App/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Header from "../Header/Header.jsx";
 import Login from "../Login/Login.jsx";
 import Footer from "../Footer/Footer.js";
-import Notifications from "../Notifications/Notifications.js";
+import Notifications from "../Notifications/Notifications.jsx";
 
 function App() {
   return (
