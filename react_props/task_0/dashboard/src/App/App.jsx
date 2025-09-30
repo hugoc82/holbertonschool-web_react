cc2a1562fd056409e4b53c278dc1184986c8f85d
hugoc82 <hugocapelle@orange.fr>
2025-09-30 11:59:32 +0200

    App.jsx: strict format for checker (single quotes; exact order; Fragment)

--- react_props/task_0/dashboard/src/App/App.jsx
+++ react_props/task_0/dashboard/src/App/App.jsx
@@ -1,8 +1,8 @@
-﻿import React, { Fragment } from "react";
-import Notifications from "../Notifications/Notifications";
-import Header from "../Header/Header";
-import Login from "../Login/Login";
-import Footer from "../Footer/Footer";
+﻿import React, { Fragment } from 'react';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
 
 export default function App() {
   return (
