cdaff1ff711aabdbc0df22d58735ab3a245d41fa
hugoc82 <hugocapelle@orange.fr>
2025-10-02 14:10:37 +0200

    .

--- react_props/task_1/dashboard/src/Footer/Footer.spec.js
+++ react_props/task_1/dashboard/src/Footer/Footer.spec.js
@@ -1,13 +1,14 @@
-﻿import React from "react";
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import Footer from "../Footer";
+﻿import { render, screen } from "@testing-library/react";
+import Header from "./Header.jsx";
 
-describe("Footer component", () => {
-  test("rend Copyright {année} - Holberton School quand isIndex=true", () => {
-    render(<Footer />);
-    const year = new Date().getFullYear();
-    const re = new RegExp(`Copyright\\s+${year}\\s+-\\s+Holberton School`, "i");
-    expect(screen.getByText(re)).toBeInTheDocument();
-  });
+test("Verify if h1 contains School dashboard", () => {
+  render(<Header />);
+  expect(
+    screen.getByRole("heading", { level: 1, name: /school dashboard/i })
+  ).toBeInTheDocument();
+});
+
+test("Verify if the image element is rendered", () => {
+  render(<Header />);
+  expect(screen.getByAltText(/holberton logo/i)).toBeInTheDocument();
 });
