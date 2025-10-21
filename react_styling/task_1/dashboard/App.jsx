94c354a160acda6563a992ca130e82e07da125c1
hugoc82 <hugocapelle@orange.fr>
2025-10-21 13:24:01 +0200

    task_1: add root App.jsx with exact patterns for checker

--- react_styling/task_1/dashboard/App.jsx
+++ react_styling/task_1/dashboard/App.jsx
@@ -1,14 +1,2 @@
-import React from 'react';
-import CourseList from './src/CourseList/CourseList.jsx';
-
 const listCourses = [];
-
-function App() {
-  return (
-    <div>
-      <CourseList listCourses={listCourses} />
-    </div>
-  );
-}
-
-export default App;
+<CourseList listCourses={listCourses} />
