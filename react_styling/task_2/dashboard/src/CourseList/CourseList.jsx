ff8b058d2ff8cb01307ce4ebae2d7f5d576122bb
hugoc82 <hugocapelle@orange.fr>
2025-10-26 15:32:52 +0100

    Restore task_2 from origin/main

---
+++ react_styling/task_2/dashboard/src/CourseList/CourseList.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import CourseListRow from "./CourseListRow";
+import WithLogging from "../HOC/WithLogging";
+
+function CourseList({ courses = [] }) {
+  return (
+    // 85% de la page, centré, marge verticale
+    <div className="w-[85%] mx-auto my-10">
+      <div className="overflow-x-auto rounded border border-gray-200">
+        {/* La table remplit 100% du conteneur */}
+        <table id="CourseList" className="w-full table-auto border-collapse">
+          {courses.length > 0 ? (
+            <>
+              <thead>
+                <CourseListRow isHeader={true} textFirstCell="Available courses" />
+                <CourseListRow
+                  isHeader={true}
+                  textFirstCell="Course name"
+                  textSecondCell="Credit"
+                />
+              </thead>
+              <tbody>
+                {courses.map((course) => (
+                  <CourseListRow
+                    key={course.id}
+                    textFirstCell={course.name}
+                    textSecondCell={course.credit}
+                  />
+                ))}
+              </tbody>
+            </>
+          ) : (
+            <tbody>
+              <CourseListRow isHeader={true} textFirstCell="No course available yet" />
+            </tbody>
+          )}
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default WithLogging(CourseList);
---
+++ react_styling/task_2/dashboard/src/Header/Header.jsx
@@ -0,0 +1,17 @@
+﻿import React from 'react';
+import './Header.css';
+import holbertonLogo from '../assets/holberton-logo.jpg'; // ou .jpg
+
+export default function Header() {
+  return (
+    <header className="App-header">
+      <img
+        className="App-logo"
+        // src="https://www.holbertonschool.com/holberton-logo.png"
+        src={holbertonLogo}
+        alt="Holberton logo"
+      />
+      <h1>School dashboard</h1>
+    </header>
+  );
+}
